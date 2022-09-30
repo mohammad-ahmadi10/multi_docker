fc2bb472a2da0bfd3adbb283afdd6686f82cc374
mohammad ahmadi <mohammad32.ahmadi@gmail.com>
2022-09-30 17:10:17 +0200

    adding to the  travis

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react', () => {
+test('renders learn rect', () => {
   /* render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument(); */
